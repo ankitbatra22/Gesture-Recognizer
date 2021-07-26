f7d79a20f74f278a2ff21fb996c4722a7583caf1
ankitbatra2 <ankitbatra2202@gmail.com>
2021-07-26 17:52:47 -0400

    updated model arch

--- model.py
+++ model.py
@@ -6,16 +6,16 @@ activation = nn.ELU()
 class Net(nn.Module):
   def __init__(self):
     super(Net, self).__init__()
-    # LAYER 1 sees (32,3,18,84,84)
+    # LAYER 1 sees (32, 3, 18, 100, 100)
     self.conv1 = self.conv_layer(3,64,(1,2,2),(1,2,2))
-    # LAYER 2 sees (32, 64, 18, 42, 42)
+    # LAYER 2 sees (32, 64, 18, 50, 50)
     self.conv2 = self.conv_layer(64,128,(1,2,2),(1,2,2))
-    # LAYER 3 sees (32, 128, 18, 21, 21)
+    # LAYER 3 sees (32, 128, 18, 25, 25)
     self.conv3 = self.conv_layer(128,256,(2,2,2),(2,2,2))
-    # LAYER 4 sees 32, 256, 9, 10, 10
+    # LAYER 4 sees (32, 256, 9, 12, 12)
     self.conv4 = self.conv_layer(256,256,(2,2,2),(2,2,2))
-    # Convoluted feature map shape (latent space): 32, 256, 4, 5, 5 so after flatten: 25600
-    self.fc1 = nn.Linear(25600,512)
+    # Convoluted feature map shape (latent space): (32, 256, 4, 6, 6) so after flatten: 256*4*6*6 = 36864
+    self.fc1 = nn.Linear(36864,512)
     self.fc2 = nn.Linear(512,512)
     self.fc3 = nn.Linear(512,256)
     self.fc4 = nn.Linear(256,11)
@@ -40,6 +40,5 @@ class Net(nn.Module):
     x = activation(self.fc3(x))
     x = self.fc4(x)
     return x
-
 
 
