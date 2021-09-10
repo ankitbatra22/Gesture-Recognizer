64d312e218e9ae4a75451589ddfa7533ec1e2421
ankitbatra2 <ankitbatra2202@gmail.com>
2021-09-10 02:22:52 -0400

    changed cv2 imshow to include mediapipe hands

--- server/server.py
+++ server/server.py
@@ -4,6 +4,10 @@ import json
 import os
 
 import cv2
+import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
 from aiohttp import web
 from aiohttp_middlewares import cors_middleware
 
@@ -33,7 +37,29 @@ async def consume_video(track):
         detector.add_frame(img)
         detector.predict_on_frames()
 
-        cv2.imshow("window", img)
+        with mp_hands.Hands(
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5) as hands:
+
+                image = cv2.cvtColor(cv2.flip(img, 1), cv2.COLOR_BGR2RGB)
+
+                image.flags.writeable = False
+                results = hands.process(image)
+
+                image.flags.writeable = True
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                if results.multi_hand_landmarks:
+                    for hand_landmarks in results.multi_hand_landmarks:
+                        mp_drawing.draw_landmarks(
+                            image,
+                            hand_landmarks,
+                            mp_hands.HAND_CONNECTIONS,
+                            mp_drawing_styles.get_default_hand_landmarks_style(),
+                            mp_drawing_styles.get_default_hand_connections_style())
+        
+        cv2.imshow('MediaPipe Hands', image)
+
+        #cv2.imshow("window", img)
         cv2.waitKey(1)
         #dataChannel.send(json.dumps({"command": "click"}))
 
